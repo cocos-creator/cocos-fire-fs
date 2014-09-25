94c6a0a5d41312c7878237e7006ce055f7e6a6b3
Johnny Wu <KarasAya@gmail.com>
2014-09-25 11:09:48 +0800

    fix runtime error

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 var Fs = require('fs');
 var Path = require('path');
 var Mkdirp = require('mkdirp');
+var FireFs = {};
 
-function _copySync ( src, dest ) {
+function copySync ( src, dest ) {
     Fs.writeFileSync(dest, Fs.readFileSync(src));
 }
 
-function _copySyncR ( src, dest ) {
+function copySyncR ( src, dest ) {
     if ( Fs.statSync(src).isDirectory() ) {
         Fs.mkdirSync(dest);
         Fs.readdirSync(src).forEach(function(name) {
-            _copySyncR ( Path.join(src, name), Path.join(dest, name) );
+            copySyncR ( Path.join(src, name), Path.join(dest, name) );
         });
     }
     else {
-        _copySync ( src, dest );
+        copySync ( src, dest );
     }
 }
 
-var FireFs = {
-    makeTreeSync: function ( path, opts ) {
-        Mkdirp.sync(path, opts);
-    },
+FireFs.makeTreeSync = function ( path, opts ) {
+    Mkdirp.sync(path, opts);
+};
 
-    makeTree: function ( path, opts, cb ) {
-        Mkdirp(path, opts, cb);
-    },
+FireFs.makeTree = function ( path, opts, cb ) {
+    Mkdirp(path, opts, cb);
+};
 
-    // a copy function just like bash's cp 
-    copySync: function ( src, dest ) {
-        if ( Fs.existsSync(src) ) {
-            if ( Fs.statSync(src).isDirectory() ) {
-                if ( Fs.existsSync(dest) && Fs.statSync(dest).isDirectory() ) {
-                    _copySyncR ( src, Path.join(dest, Path.basename(src)) );
-                }
-                else {
-                    FireFs.mkdirpSync(Path.dirname(dest));
-                    _copySyncR ( src, dest );
-                }
+// a copy function just like bash's cp 
+FireFs.copySync = function ( src, dest ) {
+    if ( Fs.existsSync(src) ) {
+        if ( Fs.statSync(src).isDirectory() ) {
+            if ( Fs.existsSync(dest) && Fs.statSync(dest).isDirectory() ) {
+                copySyncR ( src, Path.join(dest, Path.basename(src)) );
             }
             else {
-                if ( Fs.existsSync(dest) && Fs.statSync(dest).isDirectory() ) {
-                    _copySync ( src, Path.join(dest, Path.basename(src)) );
-                }
-                else {
-                    FireFs.mkdirpSync(Path.dirname(dest));
-                    _copySync ( src, dest );
-                }
+                FireFs.makeTreeSync(Path.dirname(dest));
+                copySyncR ( src, dest );
             }
         }
-    },
+        else {
+            if ( Fs.existsSync(dest) && Fs.statSync(dest).isDirectory() ) {
+                copySync ( src, Path.join(dest, Path.basename(src)) );
+            }
+            else {
+                FireFs.makeTreeSync(Path.dirname(dest));
+                copySync ( src, dest );
+            }
+        }
+    }
 };
 
 // 
