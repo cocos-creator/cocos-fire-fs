278fd95b388a14f291795382ec276c4198bdb018
Johnny Wu <KarasAya@gmail.com>
2014-09-25 14:43:21 +0800

    update

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var FireFs = require('../index');
+
+describe('TODO', function () {
+    it('should work for simple case', function () {
+        // Path.join('foo', 'bar')
+        //     .should.eql('foo/bar');
+    });
+});
