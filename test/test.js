40ef89ed19371866e18a39ff9114db56091b6795
Nan Wang <nantas@gmail.com>
2015-03-02 19:54:05 +0800

    added mocha test for new methods.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,90 @@
 var FireFs = require('../index');
+var Path = require('path');
 
-describe('TODO', function () {
-    it('should work for simple case', function () {
-        // Path.join('foo', 'bar')
-        //     .should.eql('foo/bar');
+function reset () {
+    FireFs.rimrafSync("./test/foobar/");
+    FireFs.copySync("./test/test-data/", "./test/foobar");
+}
+
+describe('FireFs', function () {
+    describe('.exists()', function() {
+        beforeEach(function () {
+            reset();
+        });
+        it('should return false when the path does not exists', function (done) {
+            FireFs.exists(Path.join('./test', 'foobar', 'bar'), function(result) {
+                result.should.eql(false);
+                done();
+            })
+        });
+        it('should return true when the path exists as a file', function (done) {
+            FireFs.exists(Path.join('./test', 'foobar', 'foo_file'), function(result) {
+                result.should.eql(true);
+                done();
+            })
+        });
+        it('should return true when the path exists as a directory', function (done) {
+            FireFs.exists(Path.join('./test', 'foobar', 'foo_dir', 'bar_dir'), function(result) {
+                result.should.eql(true);
+                done();
+            })
+        });
+    });
+    describe('.existsSync()', function() {
+        beforeEach(function () {
+            reset();
+        });
+        it('should return false when the path does not exists', function () {
+            FireFs.existsSync(Path.join('./test', 'foobar', 'bar'))
+                .should.eql(false);
+        });
+        it('should return true when the path exists as a directory', function () {
+            FireFs.existsSync(Path.join('./test', 'foobar', 'foo_dir'))
+                .should.eql(true);
+        });
+        it('should return true when the path exists as a file', function () {
+            FireFs.existsSync(Path.join('./test', 'foobar', 'foo_file'))
+                .should.eql(true);
+        })
+    });
+    describe('.isDir()', function() {
+        beforeEach(function () {
+            reset();
+        });
+        it('should return false when the path does not exists', function (done) {
+            FireFs.isDir(Path.join('./test', 'foobar', 'bar'), function(result) {
+                result.should.eql(false);
+                done();
+            })
+        });
+        it('should return false when the path exists as a file', function (done) {
+            FireFs.isDir(Path.join('./test', 'foobar', 'foo_file'), function(result) {
+                result.should.eql(false);
+                done();
+            })
+        });
+        it('should return true when the path exists as a directory', function (done) {
+            FireFs.isDir(Path.join('./test', 'foobar', 'foo_dir', 'bar_dir'), function(result) {
+                result.should.eql(true);
+                done();
+            })
+        });
+    });
+    describe('.isDirSync()', function() {
+        beforeEach(function () {
+            reset();
+        });
+        it('should return false when the path does not exists', function () {
+            FireFs.isDirSync(Path.join('./test', 'foobar', 'bar'))
+                .should.eql(false);
+        });
+        it('should return false when the path exists as a file', function () {
+            FireFs.isDirSync(Path.join('./test', 'foobar', 'foo_file'))
+                .should.eql(false);
+        });
+        it('should return true when the path exists as a directory', function () {
+            FireFs.existsSync(Path.join('./test', 'foobar', 'foo_dir', 'bar_dir'))
+                .should.eql(true);
+        })
     });
 });
