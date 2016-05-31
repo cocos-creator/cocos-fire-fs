12cc33baf42774f2360fafdb56ef7ad13258f234
Johnny Wu <KarasAya@gmail.com>
2016-05-31 14:43:17 +0800

    update to 0.2.4

--- test/test.js
+++ test/test.js
@@ -1,145 +1,190 @@
-var FireFs = require('../index');
-var Path = require('path');
-var Chai = require('chai');
+'use strict';
 
-var expect = Chai.expect;
-Chai.should();
+const fs = require('../index');
+const path = require('path');
+const tap = require('tap');
 
 function reset () {
-    FireFs.removeSync("./test/foobar/");
-    FireFs.copySync("./test/test-data/", "./test/foobar");
+  fs.removeSync("./test/foobar/");
+  fs.copySync("./test/test-data/", "./test/foobar");
 }
 
-describe('FireFs', function () {
-    describe('.exists()', function() {
-        beforeEach(function () {
-            reset();
-        });
-        it('should return false when the path does not exists', function (done) {
-            FireFs.exists(Path.join('./test', 'foobar', 'bar'), function(result) {
-                result.should.equal(false);
-                done();
-            });
-        });
-        it('should return true when the path exists as a file', function (done) {
-            FireFs.exists(Path.join('./test', 'foobar', 'foo_file'), function(result) {
-                result.should.equal(true);
-                done();
-            });
-        });
-        it('should return true when the path exists as a directory', function (done) {
-            FireFs.exists(Path.join('./test', 'foobar', 'foo_dir', 'bar_dir'), function(result) {
-                result.should.equal(true);
-                done();
-            });
-        });
-        it('should raised an TypeError when the path is undefined', function (done) {
-            try {
-                FireFs.exists( undefined, function (result) {} );
-            } catch ( err ) {
-                expect(err).to.be.instanceof(TypeError);
-                done();
-            }
-        });
-        it('should return false when the path is null', function (done) {
-            try {
-                FireFs.exists( null, function (result) {});
-            } catch ( err ) {
-                expect(err).to.be.instanceof(TypeError);
-                done();
-            }
-        });
-    });
-    describe('.existsSync()', function() {
-        beforeEach(function () {
-            reset();
-        });
-        it('should return false when the path does not exists', function () {
-            FireFs.existsSync(Path.join('./test', 'foobar', 'bar'))
-                .should.equal(false);
-        });
-        it('should return true when the path exists as a directory', function () {
-            FireFs.existsSync(Path.join('./test', 'foobar', 'foo_dir'))
-                .should.equal(true);
-        });
-        it('should return true when the path exists as a file', function () {
-            FireFs.existsSync(Path.join('./test', 'foobar', 'foo_file'))
-                .should.equal(true);
-        });
-        it('should return false when the path under a file', function () {
-            FireFs.existsSync(Path.join('./test', 'foobar', 'foo_file', 'hello_world.js'))
-                .should.equal(false);
-        });
-        it('should return false when the path is null or undefined', function () {
-            FireFs.existsSync().should.equal(false);
-            FireFs.existsSync(null).should.equal(false);
-        });
-    });
-    describe('.isDir()', function() {
-        beforeEach(function () {
-            reset();
-        });
-        it('should return false when the path does not exists', function (done) {
-            FireFs.isDir(Path.join('./test', 'foobar', 'bar'), function(err, result) {
-                result.should.equal(false);
-                done();
-            });
-        });
-        it('should return false when the path exists as a file', function (done) {
-            FireFs.isDir(Path.join('./test', 'foobar', 'foo_file'), function(err, result) {
-                result.should.equal(false);
-                done();
-            });
-        });
-        it('should return true when the path exists as a directory', function (done) {
-            FireFs.isDir(Path.join('./test', 'foobar', 'foo_dir', 'bar_dir'), function(err, result) {
-                result.should.equal(true);
-                done();
-            });
-        });
-        it('should return false when the path parameter is undefined, null or empty string', function (done) {
-            var count = 3;
-            function countDone () {
-                --count;
-                if ( count === 0 ) {
-                    done();
-                }
-            }
+tap.test('fire-fs', t => {
+  t.test('exists()', t => {
 
-            FireFs.isDir(undefined, function(err, result) {
-                result.should.equal(false);
-                countDone();
-            });
-            FireFs.isDir(null, function(err, result) {
-                result.should.equal(false);
-                countDone();
-            });
-            FireFs.isDir("", function(err, result) {
-                result.should.equal(false);
-                countDone();
-            });
-        });
+    t.beforeEach(done => {
+      reset();
+      done();
     });
-    describe('.isDirSync()', function() {
-        beforeEach(function () {
-            reset();
-        });
-        it('should return false when the path does not exists', function () {
-            FireFs.isDirSync(Path.join('./test', 'foobar', 'bar'))
-                .should.equal(false);
-        });
-        it('should return false when the path exists as a file', function () {
-            FireFs.isDirSync(Path.join('./test', 'foobar', 'foo_file'))
-                .should.equal(false);
-        });
-        it('should return true when the path exists as a directory', function () {
-            FireFs.isDirSync(Path.join('./test', 'foobar', 'foo_dir', 'bar_dir'))
-                .should.equal(true);
-        });
-        it('should return false when the path parameter is undefined, null or empty string', function () {
-            FireFs.isDirSync(undefined).should.equal(false);
-            FireFs.isDirSync(null).should.equal(false);
-            FireFs.isDirSync("").should.equal(false);
-        });
+
+    t.test('it should return false when the path does not exists', t => {
+      fs.exists(path.join('./test', 'foobar', 'bar'), result => {
+        t.equal(result, false);
+        t.end();
+      });
     });
+
+    t.test('it should return true when the path exists as a file', t => {
+      fs.exists(path.join('./test', 'foobar', 'foo_file'), result => {
+        t.equal(result, true);
+        t.end();
+      });
+    });
+
+    t.test('it should return true when the path exists as a directory', t => {
+      fs.exists(path.join('./test', 'foobar', 'foo_dir', 'bar_dir'), result => {
+        t.equal(result, true);
+        t.end();
+      });
+    });
+
+    t.test('it should raised an TypeError when the path is undefined', t => {
+      try {
+        fs.exists( undefined, result => {} );
+      } catch ( err ) {
+        t.type(err, TypeError);
+        t.end();
+      }
+    });
+
+    t.test('it should return false when the path is null', t => {
+      try {
+        fs.exists( null, result => {});
+      } catch ( err ) {
+        t.type(err, TypeError);
+        t.end();
+      }
+    });
+
+    t.end();
+  });
+
+  t.test('existsSync()', t => {
+    t.beforeEach(done => {
+      reset();
+      done();
+    });
+
+    t.test('it should return false when the path does not exists', t => {
+      let result = fs.existsSync(path.join('./test', 'foobar', 'bar'));
+
+      t.equal(result, false);
+      t.end();
+    });
+
+    t.test('it should return true when the path exists as a directory', t => {
+      let result = fs.existsSync(path.join('./test', 'foobar', 'foo_dir'));
+
+      t.equal(result, true);
+      t.end();
+    });
+
+    t.test('it should return true when the path exists as a file', t => {
+      let result = fs.existsSync(path.join('./test', 'foobar', 'foo_file'));
+
+      t.equal(result, true);
+      t.end();
+    });
+
+    t.test('it should return false when the path under a file', t => {
+      let result = fs.existsSync(path.join('./test', 'foobar', 'foo_file', 'hello_world.js'));
+
+      t.equal(result, false);
+      t.end();
+    });
+
+    t.test('it should return false when the path is null or undefined', t => {
+      t.equal(fs.existsSync(), false);
+      t.equal(fs.existsSync(null), false);
+      t.end();
+    });
+
+    t.end();
+  });
+
+  t.test('isDir()', t => {
+    t.beforeEach(done => {
+      reset();
+      done();
+    });
+
+    t.test('it should return false when the path does not exists', t => {
+      fs.isDir(path.join('./test', 'foobar', 'bar'), (err, result) => {
+        t.equal(result, false);
+        t.end();
+      });
+    });
+
+    t.test('it should return false when the path exists as a file', t => {
+      fs.isDir(path.join('./test', 'foobar', 'foo_file'), (err, result) => {
+        t.equal(result, false);
+        t.end();
+      });
+    });
+
+    t.test('it should return true when the path exists as a directory', t => {
+      fs.isDir(path.join('./test', 'foobar', 'foo_dir', 'bar_dir'), (err, result) => {
+        t.equal(result, true);
+        t.end();
+      });
+    });
+
+    t.test('it should return false when the path parameter is undefined, null or empty string', t => {
+      let count = 3;
+      function countDone () {
+        --count;
+        if ( count === 0 ) {
+          t.end();
+        }
+      }
+
+      fs.isDir(undefined, (err, result) => {
+        t.equal(result, false);
+        countDone();
+      });
+      fs.isDir(null, (err, result) => {
+        t.equal(result, false);
+        countDone();
+      });
+      fs.isDir('', (err, result) => {
+        t.equal(result, false);
+        countDone();
+      });
+    });
+
+    t.end();
+  });
+
+  t.test('isDirSync()', t => {
+    t.beforeEach(done => {
+      reset();
+      done();
+    });
+
+    t.test('it should return false when the path does not exists', t => {
+      t.equal(fs.isDirSync(path.join('./test', 'foobar', 'bar')), false);
+      t.end();
+    });
+
+    t.test('it should return false when the path exists as a file', t => {
+      t.equal(fs.isDirSync(path.join('./test', 'foobar', 'foo_file')), false);
+      t.end();
+    });
+
+    t.test('it should return true when the path exists as a directory', t => {
+      t.equal(fs.isDirSync(path.join('./test', 'foobar', 'foo_dir', 'bar_dir')), true);
+      t.end();
+    });
+
+    t.test('it should return false when the path parameter is undefined, null or empty string', t => {
+      t.equal(fs.isDirSync(undefined), false);
+      t.equal(fs.isDirSync(null), false);
+      t.equal(fs.isDirSync(''), false);
+      t.end();
+    });
+
+    t.end();
+  });
+
+  t.end();
 });
